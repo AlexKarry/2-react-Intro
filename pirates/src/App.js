7e2dda1165e55729d05cd2eb480cbed2b792ff7b
alexkarry <karyshkov@gmail.comgit config --global user.email karyshkov@gmail.comcleargit config --global user.namecleargit branchgit config user.name alexkarrygit remote add origin https://github.com/AlexKarry/git-couse.gitgit push -u origin mastergit config --global user.namegit config --global user.name>
2022-11-03 20:45:21 -0400

    second commit

--- pirates/src/App.js
+++ pirates/src/App.js
@@ -1,5 +1,9 @@
+import React from "react";
 import Header from "./components/Header";
+import AddPirate from "./components/AddPirate";
 import { Pirate } from "./components/Pirate";
+
+import piratesFile from "./data/sample-pirates-array";
 
 const pirateCalls = [
   "Aaarg, belay that!",
@@ -11,11 +15,21 @@ function randomize() {
   return pirateCalls[Math.floor(Math.random() * pirateCalls.length)];
 }
 
+//prettier-ignore
 function App() {
+  const [pirates, setPirates] = React.useState(piratesFile)
+
   return (
     <div>
       <Header title={randomize()} />
-      <Pirate name="Hello">Alex</Pirate>
+
+      <div className="pirate">
+        <AddPirate />
+        {pirates.map((pirate) => 
+          <Pirate tagline={randomize()} pirate={pirate} />
+        )}
+      </div>
+      {/* <Pirate name="Hello">Alex</Pirate> */}
     </div>
   );
 }
